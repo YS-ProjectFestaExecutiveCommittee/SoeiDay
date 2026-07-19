5cd1d5859be8b92d3dcfef9c73271a8d4ff68d41
Yokohama-Soei Project Festa Executive Committee <y.s.profes@outlook.jp>
2026-07-19 17:47:50 +0900

    Create base03.js

---
+++ javascript/base03.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+      darkMode: 'class',
+      theme: {
+        extend: {
+          colors: {
+            brand: {
+              bgLight: '#f8fafc',
+              bgDark: '#0f172a',
+              surfaceLight: '#ffffff',
+              surfaceDark: '#1e293b',
+              lime: '#a3e635',
+              limeDark: '#4d7c0f',
+              orange: '#f97316',
+              orangeDark: '#ea580c',
+            }
+          }
+        }
+      }
+    }

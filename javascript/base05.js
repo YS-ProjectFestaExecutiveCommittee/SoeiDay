ea66a6bc060fdf7ff4364af25495a59d603d2c01
Yokohama-Soei Project Festa Executive Committee <y.s.profes@outlook.jp>
2026-09-02 03:55:54 +0900

    Update base05.js

--- javascript/base05.js
+++ javascript/base05.js
@@ -2,13 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebas
 import { getFirestore, collection, onSnapshot } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
 const firebaseConfig = {
-apiKey: "AIzaSyCDWj4xWfU42x2NG1tOSlXcBC-f2vhC3lA",
-authDomain: "soeiday.firebaseapp.com",
-projectId: "soeiday",
-storageBucket: "soeiday.firebasestorage.app",
-messagingSenderId: "122257503471",
-appId: "1:122257503471:web:8014972f2bc10f84ea971a",
-measurementId: "G-EX7FJGF5R0"
+  apiKey: "AIzaSyCDWj4xWfU42x2NG1tOSlXcBC-f2vhC3lA",
+  authDomain: "soeiday.firebaseapp.com",
+  projectId: "soeiday",
+  storageBucket: "soeiday.firebasestorage.app",
+  messagingSenderId: "122257503471",
+  appId: "1:122257503471:web:8014972f2bc10f84ea971a",
+  measurementId: "G-EX7FJGF5R0"
 };
 
 const app = initializeApp(firebaseConfig);

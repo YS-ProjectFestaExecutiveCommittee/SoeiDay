43a0349a08b5a787d7d187de46064d33a0cdbfe3
Yokohama-Soei Project Festa Executive Committee <y.s.profes@outlook.jp>
2026-09-20 20:30:11 +0900

    Create firebase-config.js

---
+++ javascript/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA5uU-q3FEcElflECG6dc4AckxxX7iKj-s",
+  authDomain: "soeiday-b3b5f.firebaseapp.com",
+  projectId: "soeiday-b3b5f",
+  storageBucket: "soeiday-b3b5f.firebasestorage.app",
+  messagingSenderId: "911210293928",
+  appId: "1:911210293928:web:8ce217bfe7d5f4d837f09d",
+  measurementId: "G-1H9MSJMVQP"
+};
